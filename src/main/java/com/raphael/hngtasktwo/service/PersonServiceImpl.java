ee72c361fe08a6d996f3becac91eb9ce3bed3362
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-09-15 10:48:25 +0100

    commit

--- src/main/java/com/raphael/hngtasktwo/service/PersonServiceImpl.java
+++ src/main/java/com/raphael/hngtasktwo/service/PersonServiceImpl.java
@@ -6,7 +6,6 @@ import com.raphael.hngtasktwo.data.repository.PersonRepository;
 import com.raphael.hngtasktwo.dto.request.CreatePersonRequest;
 import com.raphael.hngtasktwo.dto.request.UpdatePersonRequest;
 import com.raphael.hngtasktwo.dto.response.ApiResponse;
-import com.raphael.hngtasktwo.exception.EmailTakenException;
 import com.raphael.hngtasktwo.exception.PersonNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
